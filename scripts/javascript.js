c2c48f0e06d8891575bf185a3f19d2b902eddf58
Gabby Cachero <gcachero29@gmail.com>
2022-07-20 11:57:33 -0500

    commit 5

---
+++ scripts/javascript.js
@@ -0,0 +1,110 @@
+//this is a comment
+/* 
+this is a comment
+*/
+ 
+// declare
+var myName; 
+
+var MYVAR;
+var MYVAR;
+var myvar;
+
+//camel case
+var myvar;
+
+//numvers,letters, $,_
+//not spaces and can't start with a number
+
+var myVar;
+var myVar;
+
+var a;
+a=55;
+//console.log(a);
+//console.log("this is a");
+
+var b
+b=a;
+
+//string
+console.log("b is equal to:" + b);
+var myLastName ="Gabby";
+var myLastName = "Cachero";
+var num = "12";
+
+
+var number ="code";
+
+var x = 4;
+var y = 11;
+var z = "I am a ";
+
+x= x+1
+y= y+5
+z=z+ "Coder"
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+
+
+
+
+
+var camper = "Gabby";
+var camper = "Asher";
+console.log(camper)
+
+let student = "Gabby";
+//let student = "Asher";
+console.log(student);
+
+
+const FAV_PET = "Dogs";
+//FAVE_PET = "Cats";
+
+const SUM = 10+10;
+const DIFFERENCE = 45 -9;
+const QUOTIENT = 66/33;
+const PRODUCT = 9*8;
+console.log(SUM);
+console.log(DIFFERENCE);
+console.log(QUOTIENT);
+console.log(PRODUCT);
+
+
+var sum = 15+10;
+console.log(sum);
+
+let sum1 = 15+1;
+console.log(sum1);
+
+let newVar= 66;
+newVar = newVar + 1;
+//equivalent to...
+newVar++
+
+console.log(newVar)
+
+newVar = newVar -1;
+//same ass...
+newVar--;
+
+let v = 6;
+v+=6;
+console.log("this is v:" +v)
+
+let s = 10;
+s-= 7;
+console.log("this is s:"+s);
+
+let m=9;
+m*=2;
+console.log("this is m:" +m);
+
+let p = 10;
+p/=5;
+console.log("this is p:" + p);
